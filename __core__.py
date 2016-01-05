9016ef3b8bd460c5564ae8cf458dbf615206c9da
David Pugh <djpugh@gmail.com>
2016-01-05 12:24:32 +0000

    Added wheel build and setup module more

--- __core__.py
+++ __core__.py
@@ -1,62 +1,8 @@
 #!/usr/bin/env python
-"""pyqsub
-=================================
-Python module for running jobs on a cluster by David J Pugh
+"""__core__
+******************
 
-This module allows any python module to be easily submitted to run on a cluster or any other job. It provides basic python wrappers for qsub and a parser group for argparse or optparse.
-
-How To Use
-*********************************
-This module is designed to be included in other modules to handle interacting with a cluster. There are two parts to the module:
-
-    1. The parser_group() function, which returns either an argparse or optparse parser group object, depending on the value of ARGPARSE, with the correct flags and destinations for the submit function.
-    2. The submit() function which uses subprocess to submit a pbs script to the cluster using qsub. 
-
-parser_group()
----------------------------------
-This function requires an appropriate parser group object depending on the value of ARGPARSE. It then adds arguments with default values as set in the function call for handling qsub jobs.
-
-To include this in your code using argparse, add the following two lines to the end of your parser code::
-
-    group=parser.add_argument_group('Cluster',description="\nCommands for using module_name on a cluster environment using qsub/PBS")
-    group=qsub.parser_group(module_name='module_name',group=group,default_nodes=default_nodes,default_ppn=default_ppn,default_pmem=default_pmem,default_walltime=default_walltime,default_queue=default_queue) 
-
-Using optparse::        
-
-    group=optparse.OptionGroup(parser,'Cluster',description="\nCommands for using  module_name on a cluster environment using qsub/PBS")
-    group=qsub.parser_group(module_name='module_name',group=group,default_nodes=default_nodes,default_ppn=default_ppn,default_pmem=default_pmem,default_walltime=default_walltime,default_queue=default_queue) 
-
-Each of the options is prefixed by qsub (e.g. qsub_nodes for the nodes argument).
-
-submit()
----------------------------------
-This function requires a dictionary of the options and the dictionary of the mappings of the option names to the command line flags (options_map). The options_map can be easily generated using an argparse parser::
-    
-    options_map={}
-    for option in parser._actions:
-        if len(option.option_strings):
-            i=0
-            while i<len(option.option_strings) and '--' not in option.option_strings[i]:
-                i+=1
-            options_map[option.dest]=option.option_strings[i]
-
-or for an optparse parser::
-    
-    options_map={}
-    for option in parser.option_list:
-        options_map[option.dest]=option.get_opt_string()
-    
-This returns an option_map that works between parsers using the longform flags --exampleflag (required for parser flags too)
-
-submit() uses the options and options_map along with the module_name to create a pbs file to run module_name from the command line using module_name.__run__().
-So the target module must have a method called __run__() that can act on sys.argv to parse and run the arguments.
-
-All flags containing qsub are ignored in constructing the script, but all other options that are in the option_map are used.
-
-submit() can take job_string as an argument instead. This is a custom job strings for the pbs script, allowing non-python programs to use the module as a simple wrapper function, or to be otherwise integrated.
-In this case, the options argument needs to contain the qsub options and the options_map argument can be a blank delimiter, while the module_name corresponds to the desired default job_name.
-
-This aspect of pyqsub can be called from the command line, try pyqsub -h for help.
+Main functions for pyqsub.
 
 -------------------------------------------
 
@@ -72,37 +18,13 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
 IN THE SOFTWARE.
-
 """
-import os,stat,subprocess,optparse,textwrap,glob
+import os,stat,subprocess,optparse,textwrap
 try:
     import argparse
     ARGPARSE=True
 except:
     ARGPARSE=False
-def isPath(string):
-    if type(string)==list:
-        for i,x in enumerate(string):
-            string[i]=isPath(x)
-        return string
-    if not string:
-        return string        
-    elif ',' in string:
-        #list
-        files=string.lstrip('[').rstrip(']').split(',')
-        for i,f in enumerate(files):
-            files[i]=isPath(f)
-        return files
-    elif string and '*' in string and os.path.exists(os.path.abspath(os.path.split(string)[0])):
-        return glob.glob(os.path.abspath(os.path.split(string)[0])+os.path.sep+os.path.split(string)[1])
-    elif string and '*' not in string and os.path.exists(os.path.abspath(string)):
-        if os.path.isdir(os.path.abspath(string)):
-            return os.path.abspath(string)+os.path.sep
-        return os.path.abspath(string)
-    if ARGPARSE:
-        raise argparse.ArgumentTypeError('Path: "'+string+'" does not exist')
-    else:
-        raise ValueError('Path: "'+string+'" does not exist')
 def make_optstr(options,options_map,list_delimiter=','):
     optstr=[]
     for key in options.keys():        
--- setup.py
+++ setup.py
@@ -1,15 +1,80 @@
 from distutils.core import setup
-
+try:
+  from setuptools import setup
+  _SETUPTOOLS=True
+except:
+  try:
+    from ez_setup import setuptools
+    _SETUPTOOLS=True
+  except:
+    _SETUPTOOLS=False
+import sys,shutil,os,datetime,subprocess,glob
 __version__='1.0.0'
-setup(
-  name = 'pyqsub',
-  packages = ['pyqsub'],
-  version = __version__,
-  description = 'Python module for running jobs on a cluster',
-  author = 'David Pugh',
-  author_email = 'djpugh@gmail.com',
-  url = 'https://github.com/djpugh/pyqsub',
-  download_url = 'https://github.com/djpugh/pyqsub/tarball/1.0.0',
-  keywords = ['cluster','torque','pbs','qsub'],
-  classifiers = [],
-)
+def setup_package(install=False,build=False,clean=False):
+    kwargs=dict(
+      name='pyqsub',
+      packages=['pyqsub'],
+      version=__version__,
+      package_dir={'pyqsub':'.'},
+      description='Python module for running jobs on a cluster',
+      author='David Pugh',
+      author_email='djpugh@gmail.com',
+      url='https://github.com/djpugh/pyqsub',
+      download_url='https://github.com/djpugh/pyqsub/tarball/1.0.0',
+      entry_points={'console_scripts': ['pyqsub = pyqsub.__init__:__run__']},
+      keywords=['cluster','torque','pbs','qsub'],
+      classifiers=[])
+    if build or 'build_all' in sys.argv or 'build-all' in sys.argv:
+        #clean dist dir
+        for fname in glob.glob('dist/*'):
+            try:
+                os.remove(fname)
+            except:
+                pass
+        print '------\nBUILDING DISTRIBUTIONS\n-----\n'
+        clean_package()
+        argv=[sys.executable,"setup.py","sdist"]
+        subprocess.call(argv)
+        clean_package()
+        argv=[sys.executable,"setup.py","sdist","--format=gztar"]
+        subprocess.call(argv)
+        clean_package()
+        argv=[sys.executable,"setup.py","bdist_wininst"]
+        subprocess.call(argv)      
+        clean_package()
+        argv=[sys.executable,"setup.py","bdist_wheel"]
+        subprocess.call(argv)      
+        clean_package()
+        argv=[sys.executable,"setup.py","bdist_msi"]
+        subprocess.call(argv)
+        clean_package()
+    elif 'clean_all' in sys.argv or clean or 'clean-all' in sys.argv:
+        argv=[sys.executable,"setup.py","clean","--all"]
+        subprocess.call(argv)
+        try:
+            shutil.rmtree('build/')
+        except:
+            pass
+        try:
+            shutil.rmtree('pyqsub-'+__version__+'/')
+            shutil.mkdir('pyqsub-'+__version__+'/')
+        except:
+            pass
+        if _SETUPTOOLS:
+            try:
+                shutil.rmtree('pyqsub.egg-info/')
+                shutil.mkdir('pyqsub.egg-info/')
+            except:
+                pass
+    else:
+        setup(**kwargs)
+def clean_package():
+    old_argv=sys.argv
+    sys.argv=['clean_all']
+    setup_package(clean=True)
+    sys.argv=old_argv
+
+if __name__=="__main__":
+    setup_package()
+    sys.stdout=sys.__stdout__
+    sys.stderr=sys.__stderr__ 
